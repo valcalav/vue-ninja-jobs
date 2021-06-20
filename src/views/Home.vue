ed8e725add848393db42bbaded6e4a0128b79b64
Veronica <veronica.alcala@gmail.com>
2021-06-20 20:07:33 +0200

    route parameters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum aut a possimus magni ducimus qui suscipit minus cumque totam, odit consequuntur corrupti rem ea excepturi debitis amet fugiat dolores! Sapiente tenetur perferendis iure fugit harum, reiciendis, praesentium dignissimos pariatur doloribus nisi dolorem distinctio animi, vel porro quia odit? Quae ut nisi sint vitae animi qui, cumque, quidem labore, eligendi exercitationem perspiciatis nesciunt voluptas repellendus in doloremque ipsam nemo facilis optio voluptate amet dolorum tenetur cum aliquid. Iste optio sit maiores possimus magni aliquid repellat laboriosam ratione iure dolore ullam quaerat, alias quod hic mollitia tempore tempora deserunt laudantium voluptates!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati explicabo animi, aspernatur totam sint ab harum cumque eos tenetur numquam, repellendus laborum. Sequi, delectus cum ipsum fugiat odio facilis.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobDetails', params: {id: job.id} }">
+        <h2>{{job.title}}</h2>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
+                {title: 'Ninja Front-end Developer', id: 2, details: 'lorem'},
+                {title: 'Ninja Back-end Developer', id: 3, details: 'lorem'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
