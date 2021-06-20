97e1c0d8f18835b76b8a886316629b260f27734b
Veronica <veronica.alcala@gmail.com>
2021-06-20 20:27:53 +0200

    programmatic navigation

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,34 @@
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
 
   </div>
+
+  <button @click="redirect" >Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view/>
 </template>
+
+<script>
+
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    back() {
+      this.$router.go(-1)
+      //We use $route to get information on the route we are currently at, and $router to do something
+    },
+    forward() {
+      this.$router.go(1)
+
+    }
+
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -33,5 +59,12 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
