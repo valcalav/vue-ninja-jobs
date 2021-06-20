1b67dc32e10ad59548bff7b54c53d81aa8347410
Veronica <veronica.alcala@gmail.com>
2021-06-20 22:04:11 +0200

    using json server and fetching data from components

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -11,12 +11,14 @@
 export default {
     data() {
         return {
-            jobs: [
-                {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-                {title: 'Ninja Front-end Developer', id: 2, details: 'lorem'},
-                {title: 'Ninja Back-end Developer', id: 3, details: 'lorem'}
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err.message))
     }
 }
 </script>
